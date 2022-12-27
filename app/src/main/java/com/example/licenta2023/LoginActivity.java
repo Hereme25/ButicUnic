1831ba3e395ab1ff5653fec63cb0a43fc20ba375
Ana Radulescu <radulescuanamaria25@yahoo.com>
2022-12-27 18:37:36 +0200

    first commit3

--- app/src/main/java/com/example/licenta2023/LoginActivity.java
+++ app/src/main/java/com/example/licenta2023/LoginActivity.java
@@ -8,29 +8,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button Conectare;
     EditText email, password;
-    TextView Butic, Unic, Inregistrare;
+    TextView Butic, Unic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Inregistrare =findViewById(R.id.Inregistrare);
         Conectare =findViewById(R.id.Conectare);
-        email =findViewById(R.id.emailEditText);
         email.setVisibility(email.INVISIBLE);
-        password =findViewById(R.id.passwordEditText);
         password.setVisibility(password.INVISIBLE);
         Butic =findViewById(R.id.textViewButic);
         Unic =findViewById(R.id.textViewUnic);
 
-        Inregistrare.setOnClickListener(new View.OnClickListener() {
+        Conectare.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
