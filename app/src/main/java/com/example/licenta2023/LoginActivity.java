5a85306b98f3403be31358ec818467aee60dfc22
Ana Radulescu <radulescuanamaria25@yahoo.com>
2023-01-12 13:51:07 +0200

    5

--- app/src/main/java/com/example/licenta2023/LoginActivity.java
+++ app/src/main/java/com/example/licenta2023/LoginActivity.java
@@ -6,30 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
-    Button Conectare;
-    TextView  Register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Conectare =findViewById(R.id.Conectare);
-        Register= findViewById(R.id.linkregister);
-
-
-
-        Register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i= new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(i);
-            }
-        });
     }
 }
--- app/src/main/java/com/example/licenta2023/MainActivity.java
+++ app/src/main/java/com/example/licenta2023/MainActivity.java
@@ -1,16 +1,38 @@
 package com.example.licenta2023;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+    Button Conectare;
+    TextView  Register;
 
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+            Conectare = findViewById(R.id.Conectare);
+            Register = findViewById(R.id.linkregister);
 
+            Conectare.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent j= new Intent(MainActivity.this, LoginActivity.class);
+                }
+            });
+            Register.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i= new Intent(MainActivity.this, RegisterActivity.class);
+                    startActivity(i);
+                }
+            });
+        }
     }
-}
